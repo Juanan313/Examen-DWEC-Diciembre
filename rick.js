f45fed80fd1ac950c567730159f47fce54fa5202
Juan Antonio Pujals Echeverría <jpujals@quonext-tur.com>
2018-12-10 17:35:33 +0100

    RicksClass

--- rick.js
+++ rick.js
@@ -1,10 +1,26 @@
 
-class Rick {
-     constructor() {
-
-     }
-
-     constructor(id) {
-         this.id = id;
-     }
+function Rick() {
+    this.messageOnTalk = "Es Rick-dículo!";
+    this.id = "C-137";
+    this.ondas = "altas";
 }
+
+Rick.prototype.habla = function() {
+    return this.messageOnTalk;
+};
+
+// Hacer el prototipo de Rick único
+function singletonRick() { 
+    const prototipo = new Rick();
+
+    return {
+        get: function() {
+            return prototipo;
+        }
+    };
+}
+
+// Exportar el módulo
+exports.rickC137 = function() {
+    return singletonRick();
+};
